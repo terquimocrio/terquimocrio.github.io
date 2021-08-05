1c0c4738cfb156369bc1ddd935e00c25ff03db30
Diego <diego_25mj@hotmail.com>
2021-08-04 23:55:20 -0700

    email to function added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
 document.querySelector(".menu-btn").addEventListener("click", () => {
-    document.querySelector(".nav-menu").classList.toggle("show");
+  document.querySelector(".nav-menu").classList.toggle("show");
 });
+
+ScrollReveal().reveal(".container", { delay: 100 });
+ScrollReveal().reveal(".home-presentation", { delay: 100 });
+ScrollReveal().reveal(".about-me", { delay: 100 });
+ScrollReveal().reveal(".about-me-illustration", { delay: 100 });
+ScrollReveal().reveal(".projects", { delay: 500 });
+ScrollReveal().reveal(".footer-contact", { delay: 100 });
+
+const name = document.getElementById("name");
+const mail = document.getElementById("mail");
+const message = document.getElementById("message")
+
+
+
+const sendMail = () =>{
+    var link = "mailto:diego_25mj@hotmail.com"
+             + `?cc=${mail.value}`
+             + "&subject=" + encodeURIComponent(`portfolio email from ${name.value}`)
+             + "&body=" + encodeURIComponent(`${message.value}`)
+    ;
+    console.log(link)
+    console.log(mail.value)
+    window.location.href = link;
+}
